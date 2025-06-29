490908268c98d1872f848f0def74d61ab27b5eb1
Lyo22 <liobear2205@gmail.com>
2025-06-29 20:06:53 -0300

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
+import vercel from "@astrojs/vercel";
 // https://astro.build/config
 
 
@@ -12,9 +13,13 @@ export default defineConfig({
   
  },
 },
+
   publicDir: "./public",
   integrations: [preact()],
-   vite: {
-    plugins: [tailwindcss()],
-  },
+
+  vite: {
+   plugins: [tailwindcss()],
+ },
+
+  adapter: vercel(),
 });
