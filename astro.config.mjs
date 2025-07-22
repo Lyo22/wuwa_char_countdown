8a949376af14c62cbb2d19d1a8309afecfcae5df
Lyo22 <liobear2205@gmail.com>
2025-07-22 02:28:37 -0300

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,14 @@ import vercel from "@astrojs/vercel";
 
 
 export default defineConfig({
-  site: import.meta.env.BASE_URL, 
+  //site: import.meta.env.BASE_URL, 
   env: {
  schema: {
    PUBLIC_PAGE_URL: envField.string({ context: "client", access: "public"}),
  },
 },
 
-  publicDir: "./public",
+  publicDir: "./public/assets",
   integrations: [preact()],
 
   vite: {
