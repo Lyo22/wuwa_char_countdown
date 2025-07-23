037f54bd599e648f76d55dc6bd19a71176ad2ec2
Lyo22 <liobear2205@gmail.com>
2025-07-23 16:29:28 -0300

    ...

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_C4GOVFQP.mjs';
-import { manifest } from './manifest_BvAt2pbn.mjs';
+import { manifest } from './manifest_Bp0OwY0e.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "5f767fc3-ead9-443f-a27b-6e47a670bdf0",
+    "middlewareSecret": "a61f31a9-a412-4e2e-ba92-b91d44ad16a8",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
