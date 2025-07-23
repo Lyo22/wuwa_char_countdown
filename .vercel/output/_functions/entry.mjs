f85962794dbd9b4883e0c812b053bfe25e875445
Lyo22 <liobear2205@gmail.com>
2025-07-23 16:31:51 -0300

    ...

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_C4GOVFQP.mjs';
-import { manifest } from './manifest_Bp0OwY0e.mjs';
+import { manifest } from './manifest_CQtnzV12.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "a61f31a9-a412-4e2e-ba92-b91d44ad16a8",
+    "middlewareSecret": "f6d2d7f7-5579-443e-af6a-f6bc661db2af",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 
 export default defineConfig({
-  output: 'server',
    adapter: vercel(),
  
   //site: import.meta.env.BASE_URL, 
