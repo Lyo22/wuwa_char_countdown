d4fff1b653de277502c63b7dea9c2bfe6e43cf5c
Lyo22 <liobear2205@gmail.com>
2025-07-23 16:14:40 -0300

    ...

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_B_7Zy6WH.mjs';
-import { manifest } from './manifest_BdtRmL1Q.mjs';
+import { manifest } from './manifest_DXJHCFIN.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "136ddd13-ae99-4962-830c-dc588b7f74d2",
+    "middlewareSecret": "33f92046-6799-4f6e-9aec-99c793f2eb66",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,16 @@ import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
 import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 
-
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   //site: import.meta.env.BASE_URL, 
   publicDir: "./public",
   integrations: [preact()],
   vite: {
    plugins: [tailwindcss()],
  },
-output: 'server',
-  adapter: vercel({
-    imageService: true,
-    webAnalytics: {
-      enabled: true,
-    }})
+
 });
