6bd6dc10be7a01ea0210d866d1552fa68d616aaa
Lyo22 <liobear2205@gmail.com>
2025-07-23 14:55:23 -0300

    update: script version.py

--- src/utils/version.py
+++ src/utils/version.py
@@ -2,25 +2,23 @@ import asyncio
 from playwright.async_api import async_playwright
 import re
 
-
-url = "https://wutheringwaves.kurogames.com/en/main"
+url = "https://wutheringwaves.kurogames.com/en/"
 
 async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True) 
         page = await browser.new_page()
         await page.goto(url)
-        await page.wait_for_selector("div.banner-text", timeout=10000) 
-
-        element = await page.query_selector("div.banner-text")
-        if element:
-            text = await element.inner_text()
-            match = re.search(r"\d+\.\d+", text)
-            if match: 
-                return match.group()
-        else:
-            print("No se encontró el elemento.")
-          
+        container = await page.wait_for_selector("div.video-container", timeout=10000) 
+        if container is not None:
+            element = await container.query_selector("img[alt='']") 
+            if element is not None:
+                text = await element.get_attribute("src")
+                v = text.split("=")[1] if text is not None else ""  
+                print(v)
+                return v
+            else:
+                print("No se encontró el elemento.")
         await browser.close()
 
 asyncio.run(main())
